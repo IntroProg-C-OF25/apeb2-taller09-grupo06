b212db04520a1a0c2227c24e6ca7891b8130156f
Dagonzález2006 <dudef.2222.11@gmail.com>
2024-12-17 19:47:39 -0500

    Resolucion de los ejercicios tiempo= 4.15 horas

--- apeb2-taller09-grupo06/src/Ejercicio1_figura1.java
+++ apeb2-taller09-grupo06/src/Ejercicio1_figura1.java
@@ -1,12 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Scanner;
 
 /**
- *
- * @author Usuario
+ * Con el objetivo practicar el uso del ciclo repetitivo for, se propone que dado un límite, 
+ * se presenten las siguientes figuras. Por ejemplo si el límite es 4:
+ * Figura 1:
+ *    *
+ *    **
+ *    ***
+ *    ****
+ * @author David González / Dudef-ui
  */
 public class Ejercicio1_figura1 {
-    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int lim;
+        System.out.println("Dime el nivel del arbol: ");
+        lim = teclado.nextInt();
+        for (int i = 1; i < lim; i++) {
+            for (int j = 0; j < (i-1) ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
 }
+/***
+ * run:
+ * Dime el nivel del arbol: 
+ * 5
+ * *
+ * **
+ * ***
+ * BUILD SUCCESSFUL (total time: 2 seconds)
+ */
+    
+
--- apeb2-taller09-grupo06/src/Ejercicio2.java
+++ apeb2-taller09-grupo06/src/Ejercicio2.java
@@ -1,12 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Scanner;
 
 /**
+ * Ejercicio 2 Escriba un programa que permita presentar la siguiente serie:
+ * 1/2, 1/3, 2/5, 3/7, 5/11, 8/13, . . . fibonaccis / primos
  *
  * @author Usuario
  */
 public class Ejercicio2 {
-    
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int lim, a = 0, b = 1, c = 0;
+        int cant = 10, cont = 0, num = 2, i = 2;
+        System.out.print("Dime cuantos numeros quieres visualizar: ");
+        lim = teclado.nextInt();
+        int[] fibo = new int[lim];
+        for (i = 0; i < lim; i++) {
+            //CADENA FIBO
+            a = b;
+            b = c;
+            c = a + b;
+            fibo[i] = c;
+        }
+        //DETERMINAR SI UN NUMERO ES PRIMO O NO
+        int[] primos = new int[lim];
+        while (cont < cant) {
+            for (i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    break;
+                }
+            }
+            if (i > Math.sqrt(num)) {
+                primos[cont] = num;
+                cont++;
+            }
+            num++;
+        }
+        for ( i = 0; i < lim; i++) {
+            System.out.print(fibo[i]+"/"+primos[i] + " ");
+        }
+    }
 }
+/***
+ * run:
+ * Dime cuantos numeros quieres visualizar: 10
+ * 1/2 1/3 2/5 3/7 5/11 8/13 13/17 21/19 34/23 55/29 BUILD SUCCESSFUL (total time: 3 seconds)
+ */
